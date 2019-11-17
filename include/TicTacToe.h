81ee3f117bee7278d73d2a3548f401fc568e4615
Fernando Rosendo <franplt629@gmail.com>
2019-11-17 14:22:10 -0300

    button 1 done

--- include/TicTacToe.h
+++ include/TicTacToe.h
@@ -2,6 +2,7 @@
 #define TICTACTOE_H
 
 #include <QMainWindow>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TicTacToe; }
@@ -12,10 +13,23 @@ class TicTacToe : public QMainWindow
     Q_OBJECT
 
 public:
+    enum Player{
+        PLAYER1,
+        PLAYER2
+    };
+
+    Player player;
+
+public:
     TicTacToe(QWidget *parent = nullptr);
     ~TicTacToe();
 
+private slots:
+    void on_pushButton_1_clicked();
+
 private:
     Ui::TicTacToe *ui;
+    void pressButton(QPushButton& button);
+    void togglePlayer();
 };
 #endif // TICTACTOE_H
--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -3,6 +3,7 @@
 
 TicTacToe::TicTacToe(QWidget *parent)
     : QMainWindow(parent)
+    , player(PLAYER1)
     , ui(new Ui::TicTacToe)
 {
     ui->setupUi(this);
@@ -13,3 +14,22 @@ TicTacToe::~TicTacToe()
     delete ui;
 }
 
+void TicTacToe::togglePlayer(){
+    if(player == PLAYER1){
+        player = PLAYER2;
+        return;
+    }
+    player = PLAYER1;
+}
+
+void TicTacToe::pressButton(QPushButton &button){
+    if(player == PLAYER1)
+        button.setText("X");
+    else
+        button.setText("O");
+}
+
+void TicTacToe::on_pushButton_1_clicked()
+{
+    pressButton(*(ui->pushButton_1));
+}
